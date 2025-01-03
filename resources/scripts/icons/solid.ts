89c1593d4e651f449b8021fa38290f2db6ce1d97
Alex Torscho <atorscho@users.noreply.github.com>
2025-01-03 19:18:31 +0100

    Update solid.ts

--- resources/scripts/icons/solid.ts
+++ resources/scripts/icons/solid.ts
@@ -1,31 +1,5 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
 
-import {
-    faArrowRight,
-    faBook,
-    faBookOpen,
-    faCaretDown,
-    faCircleRight,
-    faFileChartColumn,
-    faFileCode,
-    faFileInvoice,
-    faFireFlameCurved,
-    faGauge,
-    faGlobe,
-    faUsers,
-} from '@fortawesome/pro-solid-svg-icons'
+// import {} from '@fortawesome/pro-solid-svg-icons'
 
-library.add(
-    faArrowRight,
-    faBook,
-    faBookOpen,
-    faCaretDown,
-    faCircleRight,
-    faFileChartColumn,
-    faFileCode,
-    faFileInvoice,
-    faFireFlameCurved,
-    faGauge,
-    faGlobe,
-    faUsers,
-)
+// library.add()
