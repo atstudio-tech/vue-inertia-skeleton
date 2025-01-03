de9cfcc6180e5cba40c25734edc037e167f9d739
Alex Torscho <atorscho@users.noreply.github.com>
2025-01-03 12:54:03 +0100

    Update Icon.vue

--- resources/views/Components/Icon.vue
+++ resources/views/Components/Icon.vue
@@ -5,7 +5,6 @@
 <script lang="ts" setup>
 import { IconName, IconPrefix } from '@fortawesome/fontawesome-common-types'
 import { icon as faIcon } from '@fortawesome/fontawesome-svg-core'
-import { computed } from 'vue'
 
 const {
     prefix = 'fal',
